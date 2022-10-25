5aed95ba559a47ea4b4bf172a5a4faef1ccdcd57
jayanta-musquare <76578225+jayanta-developer@users.noreply.github.com>
2022-10-25 14:55:33 +0530

    YouTube clone
    Make a YouTube clone application

---
+++ src/component/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchBar from "./searchBar";
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+      hello from app component
+    </div>
+  );
+};
+
+export default App;
