153de1aba478acfab8dc310411878d4b276af39f
jayanta dey <jayantadey398@gmail.com>
2022-11-10 23:25:53 +0530

    change the label

--- src/component/searchBar.jsx
+++ src/component/searchBar.jsx
@@ -1,25 +1,22 @@
 import React, { useState } from "react";
 
 const SearchBar = ({ onFromSubmit }) => {
-  const [term, setTerm] = useState('')
+  const [term, setTerm] = useState("");
 
   const fromSubmit = (e) => {
     e.preventDefault();
-    onFromSubmit(term)
-  }
+    onFromSubmit(term);
+  };
 
   return (
     <div className="search-bar ui segment">
       <form className="ui form" onSubmit={fromSubmit}>
         <div className="field">
-          <label> Video Search</label>
-          <input value={term}
-            onChange={e => setTerm(e.target.value)}
-          />
+          <label>Search for a video</label>
+          <input value={term} onChange={(e) => setTerm(e.target.value)} />
         </div>
       </form>
     </div>
-  )
-
-}
+  );
+};
 export default SearchBar;
